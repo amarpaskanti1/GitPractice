3358172f93a5ff2dc39838f126a63fdf50874b55
amarpaskanti1 <amarpaskanti1@gmail.com>
2021-07-08 11:26:45 +0530

    First Commit....

---
+++ src/main/java/Git/GitPractice/App.java
@@ -0,0 +1,13 @@
+package Git.GitPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
